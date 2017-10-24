0ec9f52d6fc622ecd344c65b3ccfde4f3af5ed83
Tanuj Garg <Tanujgarg@y7mail.com>
2017-10-24 20:47:10 +0530

    adding a song

--- Songify/main.js
+++ Songify/main.js
@@ -1,13 +1,52 @@
-/**
- * Created by tanuj on 10/11/17.
- */
-var name = document.querySelector("#name-input").value;
-function myfunc() {
-        var name = document.querySelector("#name-input").value;
-        message = "Welcome " + name;
-        console.log(name);
-        $('.welcome-screen').addClass('hidden');
+$('#myform').on('submit', function() {
+        var name = $('#name-input').val();
+        var message = "Welcome, " + name;
+        $('.main .user-name h3').text(message.toUpperCase());
+        //$('.welcome-screen').addClass('hidden');
+        $(".welcome-screen").remove();
         $('.main').removeClass('hidden');
-        $('.main h1').text(message);
+
+
     }
-document.getElementById("myform").onsubmit = function() {myfunc()};
+);
+$('.play-icon,#song').on('click', function togglesong() {
+    var song = document.querySelector('audio');
+    if (song.paused == true) {
+        console.log('Playing');
+        $('.play-icon').removeClass('fa-play').addClass('fa-pause');
+        song.play();
+    } else {
+        console.log('Pausing');
+        $('.play-icon').removeClass('fa-pause').addClass('fa-play');
+        song.pause();
+    }
+});
+$('body').on('keypress', function(event) {
+    if ($("#main").hasClass('hidden')){
+        console.log("...");
+    }
+    else {
+        if (event.keyCode == 32) {
+            var song = document.querySelector('audio');
+            if (song.paused == true) {
+                console.log('Playing');
+                $('.play-icon').removeClass('fa-play').addClass('fa-pause');
+                song.play();
+            } else {
+                console.log('Pausing');
+                $('.play-icon').removeClass('fa-pause').addClass('fa-play');
+                song.pause();
+            }
+
+        }
+    }
+});
+var songName='Tamma Tamma';
+var songArtist ='Anuradha Paudwal, Badshah, Bappi Lahiri.';
+var songAlbum = 'Badrinath Ki Dulhaniya';
+var song = document.querySelector('audio');
+var duration = song.duration.toString();
+$('#song .song-name').text(songName);
+$('#song .song-artist').text(songArtist);
+$('#song .song-album').text(songAlbum);
+$('#song .song-duration').text(duration);
